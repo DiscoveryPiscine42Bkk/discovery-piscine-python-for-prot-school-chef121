20e25b2593d597e81bf1d3481996786754fce283
chef121 <40507@prot.ac.th>
2025-03-10 14:54:25 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,9 @@
+correct_password =  "Python is awesome"
+
+user_input = input("Enter password: ")  
+
+if user_input == correct_password:
+    print("ACCESS GRANTED") 
+else: 
+    print("ACCESS DENIED")
+
