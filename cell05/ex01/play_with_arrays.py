a8cca44b3bb7c9991c0c414b292e60fb5f12572e
chef121 <40507@prot.ac.th>
2025-03-11 12:38:04 +0700

    Add files via upload

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,8 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+# สร้างอาร์เรย์ใหม่โดยบวก 2 กับแต่ละค่าในอาร์เรย์เดิม
+new_array = [x + 2 for x in original_array]
+
+# แสดงผลลัพธ์
+print("Original array:", original_array)
+print("New array:", new_array)
