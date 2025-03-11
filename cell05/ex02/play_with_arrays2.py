6411ac08d0df8b6427e4655c0df505a80b395c74
chef121 <40507@prot.ac.th>
2025-03-11 13:13:19 +0700

    Add files via upload

---
+++ cell05/ex02/play_with_arrays2.py
@@ -0,0 +1,11 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+# กรองเฉพาะค่าที่มากกว่า 5 และสร้างอาร์เรย์ใหม่โดยบวก 2 กับค่าเหล่านั้น
+new_array = [x + 2 for x in original_array if x > 5]
+
+# ลบค่าซ้ำโดยใช้ set() แล้วแปลงกลับเป็น list
+unique_new_array = list(set(new_array))
+
+# แสดงผลลัพธ์
+print(original_array)
+print(unique_new_array)
